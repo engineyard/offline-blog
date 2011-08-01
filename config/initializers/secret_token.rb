0959b993c20952650605d864d04ce892db834c65
Chris Tosswill <ctosswill@engineyard.com>
2011-08-01 16:02:01 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offline::Application.config.secret_token = 'c1f18d07f3af61a8be06f51889069a238e90d440b5811f49762d74fa0e68bf7f582a7b3ea262b988ca738e80a0b5738799c76ed7f1dadd0ef5c7208686d52d90'
